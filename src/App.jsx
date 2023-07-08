d9bd630f54fbee1195b94339a5e3701bb7c4fbb2
Abd-Elaty <ibrahimdefala@gmail.com>
2023-07-09 00:47:51 +0300

    add more elements and edit style

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import Header from './components/Header'
 import Categories from './components/Categories'
+import Footer from './components/Footer'
 import './styles/App.scss'
 import { useState } from 'react'
 
 const foods = [
   {category:'Fruits', price:'5$', stocked:true, name:'Apple', image:'/src/assets/Apple.png'},
   {category:'Fruits', price:'10$', stocked:false, name:'Star Fruit', image:'/src/assets/starfruit.png'},
-  {category:'Fruits', price:'3$', stocked:true, name:'Star Fruit', image:'/src/assets/strawberry.png'},
+  {category:'Fruits', price:'3$', stocked:true, name:'StarFruit', image:'/src/assets/strawberry.png'},
   {category:'Vegetables', price:'8$', stocked:true, name:'Potato', image:'/src/assets/Potato.png'},
   {category:'Fruits', price:'7$', stocked:true, name:'Abricot', image:'/src/assets/Abricot.png'},
   {category:'Vegetables', price:'3$', stocked:true, name:'Spinach', image:'/src/assets/Spinach.png'},
   {category:'Vegetables', price:'6$', stocked:false, name:'Pumpkin', image:'/src/assets/Pumpkin.png'},
+  {category:'Vegetables', price:'2$', stocked:true, name:'Onion', image:'/src/assets/onion.png'},
+  {category:'Vegetables', price:'4$', stocked:true, name:'Tomato', image:'/src/assets/tomato.png'},
   {category:'Fruits', price:'12$', stocked:true, name:'Orange', image:'/src/assets/Orange.png'},
+  {category:'Bakeries', price:'20$', stocked:true, name:'Banana', image:'/src/assets/Banana.png'},
+  {category:'Bakeries', price:'9$', stocked:true, name:'Baguette', image:'/src/assets/Baguette.png'},
+  {category:'Bakeries', price:'12$', stocked:false, name:'Breadstick', image:'/src/assets/Breadstick.png'},
+  {category:'Bakeries', price:'12$', stocked:true, name:'Brioche', image:'/src/assets/Brioche.png'},
+  {category:'Bakeries', price:'22$', stocked:true, name:'Challah', image:'/src/assets/Challah.png'},
 ]
 
 function App() {
@@ -22,6 +30,7 @@ function App() {
     <>
       <Header filt={filt} setFilt={setFilt} search={search} setSearch={setSearch}/>
       <Categories Foods={foods} filt={filt} search={search}/>
+      <Footer/>
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import '../styles/footer.scss'
+
+function Footer () {
+  return (
+    <footer>
+      Created by Ibrahim Abd Elaty
+      <a href="https://github.com/Abd-Elaty" target='_blank'>
+        <img src="/src/assets/github.png"/>
+      </a>
+      <a href="https://www.linkedin.com/in/ibrahim-abd-elaty-633025278/" target='_blank'>
+        <img src="/src/assets/linkedin.png"/>
+      </a>
+    </footer>
+  )
+}
+export default Footer
